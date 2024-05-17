10355ad5b2bbb266116111d6ececda0b3eeaf4cb
ShpatzR <roni1kohen@gmail.com>
2024-05-17 22:11:07 +0100

    start games

--- testanimation.js
+++ testanimation.js
@@ -1,4 +1,8 @@
-
+const hill1 = document.getElementsByClassName("hill-1");
+const kid = document.getElementsByClassName("kid-medi");
+const hill2 = document.getElementsByClassName("hill-2");
+const hill3 = document.getElementsByClassName("hill-3");
+const hill4 = document.getElementsByClassName("hill-4");
 
 const options = {
     root: null,
@@ -9,9 +13,15 @@ const options = {
 const callback = (entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting){
-            console.log("its in")
-            entry.target.classList.add("in")
+            // console.log("its in")
+            hill1[0].classList.add("hill-1-in");
+            kid[0].classList.add("hill-1-in");
+            hill2[0].classList.add("hill-1-in");
+            hill3[0].classList.add("hill-2-in");
+            hill4[0].classList.add("hill-2-in");
+           
             observer.unobserve(entry.target)
+           
         }
     });
 }
