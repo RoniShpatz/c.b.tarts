397e44b63f5771ae3b9f67e8fa12e507db5fe82c
ShpatzR <roni1kohen@gmail.com>
2024-05-21 20:26:47 +0100

    fix games-js

--- wheel.js
+++ wheel.js
@@ -3,6 +3,17 @@ const solution = document.getElementById("solution");
 const submitProblem = document.getElementById("submit-sproblem");
 const addSolution = document.getElementById("add-solution");
 const wheelParent = document.getElementById("wheel");
+const btnSpin = document.getElementById("spin-btn");
+const finalValeu = document.getElementById("final-valeu");
+const rotationValues = [
+    {minDegree: 0, maxDegree: 30, value: 2},
+    {minDegree: 31, maxDegree: 90, value: 1},
+    {minDegree: 91, maxDegree: 150, value: 2},
+    {minDegree: 151, maxDegree: 210, value: 2},
+    {minDegree: 211, maxDegree: 270, value: 2},
+    {minDegree: 271, maxDegree: 30, value: 2},
+    {minDegree: 0, maxDegree: 30, value: 2},
+];
 let arryOfSolutions = [];
 
 
